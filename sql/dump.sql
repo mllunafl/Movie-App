656f8b285edd524658be532f747cc09de3a7b91d
Maira <mllunaflores@gmail.com>
2017-02-13 14:07:58 -0600

    dump

---
+++ sql/dump.sql
@@ -0,0 +1,337 @@
+-- MySQL dump 10.13  Distrib 5.7.16, for osx10.11 (x86_64)
+--
+-- Host: localhost    Database: sandbox
+-- ------------------------------------------------------
+-- Server version	5.7.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `DATABASECHANGELOG`
+--
+
+DROP TABLE IF EXISTS `DATABASECHANGELOG`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `DATABASECHANGELOG` (
+  `ID` varchar(255) NOT NULL,
+  `AUTHOR` varchar(255) NOT NULL,
+  `FILENAME` varchar(255) NOT NULL,
+  `DATEEXECUTED` datetime NOT NULL,
+  `ORDEREXECUTED` int(11) NOT NULL,
+  `EXECTYPE` varchar(10) NOT NULL,
+  `MD5SUM` varchar(35) DEFAULT NULL,
+  `DESCRIPTION` varchar(255) DEFAULT NULL,
+  `COMMENTS` varchar(255) DEFAULT NULL,
+  `TAG` varchar(255) DEFAULT NULL,
+  `LIQUIBASE` varchar(20) DEFAULT NULL,
+  `CONTEXTS` varchar(255) DEFAULT NULL,
+  `LABELS` varchar(255) DEFAULT NULL,
+  `DEPLOYMENT_ID` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `DATABASECHANGELOG`
+--
+
+LOCK TABLES `DATABASECHANGELOG` WRITE;
+/*!40000 ALTER TABLE `DATABASECHANGELOG` DISABLE KEYS */;
+INSERT INTO `DATABASECHANGELOG` VALUES ('00000000000001','jhipster','classpath:config/liquibase/changelog/00000000000000_initial_schema.xml','2017-02-07 16:01:22',1,'EXECUTED','7:a91966e379c49b41dd5b60b86be5e082','createTable tableName=jhi_user; createIndex indexName=idx_user_login, tableName=jhi_user; createIndex indexName=idx_user_email, tableName=jhi_user; createTable tableName=jhi_authority; createTable tableName=jhi_user_authority; addPrimaryKey tableN...','',NULL,'3.5.3',NULL,NULL,'6504845181'),('20170207220703-1','jhipster','classpath:config/liquibase/changelog/20170207220703_added_entity_Movie.xml','2017-02-07 16:16:16',2,'EXECUTED','7:f07921cef68212df00a2247f4d7e6c7e','createTable tableName=movie','',NULL,'3.5.3',NULL,NULL,'6505773515'),('20170213161108-1','jhipster','classpath:config/liquibase/changelog/20170213161108_added_entity_Movie_wishlist.xml','2017-02-13 10:23:28',3,'EXECUTED','7:e09e0d0a3cdec1ab7dc71c80dea3371a','createTable tableName=movie_wishlist; createTable tableName=movie_wishlist_movie; addPrimaryKey tableName=movie_wishlist_movie','',NULL,'3.5.3',NULL,NULL,'7003005207'),('20170213161108-2','jhipster','classpath:config/liquibase/changelog/20170213161108_added_entity_constraints_Movie_wishlist.xml','2017-02-13 10:23:29',4,'EXECUTED','7:597017c19d2cb22621d5db7287bcb5d3','addForeignKeyConstraint baseTableName=movie_wishlist_movie, constraintName=fk_movie_wishlist_movie_movie_wishlists_id, referencedTableName=movie_wishlist; addForeignKeyConstraint baseTableName=movie_wishlist_movie, constraintName=fk_movie_wishlist...','',NULL,'3.5.3',NULL,NULL,'7003005207');
+/*!40000 ALTER TABLE `DATABASECHANGELOG` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `DATABASECHANGELOGLOCK`
+--
+
+DROP TABLE IF EXISTS `DATABASECHANGELOGLOCK`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `DATABASECHANGELOGLOCK` (
+  `ID` int(11) NOT NULL,
+  `LOCKED` bit(1) NOT NULL,
+  `LOCKGRANTED` datetime DEFAULT NULL,
+  `LOCKEDBY` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `DATABASECHANGELOGLOCK`
+--
+
+LOCK TABLES `DATABASECHANGELOGLOCK` WRITE;
+/*!40000 ALTER TABLE `DATABASECHANGELOGLOCK` DISABLE KEYS */;
+INSERT INTO `DATABASECHANGELOGLOCK` VALUES (1,'\0',NULL,NULL);
+/*!40000 ALTER TABLE `DATABASECHANGELOGLOCK` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jhi_authority`
+--
+
+DROP TABLE IF EXISTS `jhi_authority`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jhi_authority` (
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jhi_authority`
+--
+
+LOCK TABLES `jhi_authority` WRITE;
+/*!40000 ALTER TABLE `jhi_authority` DISABLE KEYS */;
+INSERT INTO `jhi_authority` VALUES ('ROLE_ADMIN'),('ROLE_USER');
+/*!40000 ALTER TABLE `jhi_authority` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jhi_persistent_audit_event`
+--
+
+DROP TABLE IF EXISTS `jhi_persistent_audit_event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jhi_persistent_audit_event` (
+  `event_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `principal` varchar(50) NOT NULL,
+  `event_date` timestamp NULL DEFAULT NULL,
+  `event_type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`event_id`),
+  KEY `idx_persistent_audit_event` (`principal`,`event_date`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jhi_persistent_audit_event`
+--
+
+LOCK TABLES `jhi_persistent_audit_event` WRITE;
+/*!40000 ALTER TABLE `jhi_persistent_audit_event` DISABLE KEYS */;
+INSERT INTO `jhi_persistent_audit_event` VALUES (1,'admin','2017-02-07 22:01:54','AUTHENTICATION_SUCCESS'),(2,'admin','2017-02-07 22:01:54','AUTHENTICATION_SUCCESS');
+/*!40000 ALTER TABLE `jhi_persistent_audit_event` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jhi_persistent_audit_evt_data`
+--
+
+DROP TABLE IF EXISTS `jhi_persistent_audit_evt_data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jhi_persistent_audit_evt_data` (
+  `event_id` bigint(20) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`event_id`,`name`),
+  KEY `idx_persistent_audit_evt_data` (`event_id`),
+  CONSTRAINT `fk_evt_pers_audit_evt_data` FOREIGN KEY (`event_id`) REFERENCES `jhi_persistent_audit_event` (`event_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jhi_persistent_audit_evt_data`
+--
+
+LOCK TABLES `jhi_persistent_audit_evt_data` WRITE;
+/*!40000 ALTER TABLE `jhi_persistent_audit_evt_data` DISABLE KEYS */;
+INSERT INTO `jhi_persistent_audit_evt_data` VALUES (1,'remoteAddress','0:0:0:0:0:0:0:1'),(2,'remoteAddress','0:0:0:0:0:0:0:1');
+/*!40000 ALTER TABLE `jhi_persistent_audit_evt_data` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jhi_persistent_token`
+--
+
+DROP TABLE IF EXISTS `jhi_persistent_token`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jhi_persistent_token` (
+  `series` varchar(20) NOT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `token_value` varchar(20) NOT NULL,
+  `token_date` date DEFAULT NULL,
+  `ip_address` varchar(39) DEFAULT NULL,
+  `user_agent` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`series`),
+  KEY `fk_user_persistent_token` (`user_id`),
+  CONSTRAINT `fk_user_persistent_token` FOREIGN KEY (`user_id`) REFERENCES `jhi_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jhi_persistent_token`
+--
+
+LOCK TABLES `jhi_persistent_token` WRITE;
+/*!40000 ALTER TABLE `jhi_persistent_token` DISABLE KEYS */;
+INSERT INTO `jhi_persistent_token` VALUES ('e1ArUuTk6XMqyOoTJ3Oh',3,'XmtxJka4mbJdiNocN42R','2017-02-09','0:0:0:0:0:0:0:1','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'),('RaLv0P8iks4wrqes9bBk',3,'mnUSZd2UDwnmXc28ndyU','2017-02-07','0:0:0:0:0:0:0:1','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36');
+/*!40000 ALTER TABLE `jhi_persistent_token` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jhi_user`
+--
+
+DROP TABLE IF EXISTS `jhi_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jhi_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `login` varchar(50) NOT NULL,
+  `password_hash` varchar(60) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `image_url` varchar(256) DEFAULT NULL,
+  `activated` bit(1) NOT NULL,
+  `lang_key` varchar(5) DEFAULT NULL,
+  `activation_key` varchar(20) DEFAULT NULL,
+  `reset_key` varchar(20) DEFAULT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `created_date` timestamp NOT NULL,
+  `reset_date` timestamp NULL DEFAULT NULL,
+  `last_modified_by` varchar(50) DEFAULT NULL,
+  `last_modified_date` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login` (`login`),
+  UNIQUE KEY `idx_user_login` (`login`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `idx_user_email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jhi_user`
+--
+
+LOCK TABLES `jhi_user` WRITE;
+/*!40000 ALTER TABLE `jhi_user` DISABLE KEYS */;
+INSERT INTO `jhi_user` VALUES (1,'system','$2a$10$mE.qmcV0mFU5NcKh73TZx.z4ueI/.bDWbj0T1BYyqP481kGGarKLG','System','System','system@localhost','','','en',NULL,NULL,'system','2017-02-07 22:00:57',NULL,'system',NULL),(2,'anonymoususer','$2a$10$j8S5d7Sr7.8VTOYNviDPOeWX8KcYILUVJBsYV83Y5NtECayypx9lO','Anonymous','User','anonymous@localhost','','','en',NULL,NULL,'system','2017-02-07 22:00:57',NULL,'system',NULL),(3,'admin','$2a$10$gSAhZrxMllrbgj/kkK9UceBPpChGWJA7SYIb1Mqo.n5aNLq1/oRrC','Administrator','Administrator','admin@localhost','','','en',NULL,NULL,'system','2017-02-07 22:00:57',NULL,'system',NULL),(4,'user','$2a$10$VEjxo0jq2YG9Rbk2HmX9S.k1uZBGYUHdUcid3g/vfiEl7lwWgOH/K','User','User','user@localhost','','','en',NULL,NULL,'system','2017-02-07 22:00:57',NULL,'system',NULL);
+/*!40000 ALTER TABLE `jhi_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jhi_user_authority`
+--
+
+DROP TABLE IF EXISTS `jhi_user_authority`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jhi_user_authority` (
+  `user_id` bigint(20) NOT NULL,
+  `authority_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`user_id`,`authority_name`),
+  KEY `fk_authority_name` (`authority_name`),
+  CONSTRAINT `fk_authority_name` FOREIGN KEY (`authority_name`) REFERENCES `jhi_authority` (`name`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `jhi_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jhi_user_authority`
+--
+
+LOCK TABLES `jhi_user_authority` WRITE;
+/*!40000 ALTER TABLE `jhi_user_authority` DISABLE KEYS */;
+INSERT INTO `jhi_user_authority` VALUES (1,'ROLE_ADMIN'),(3,'ROLE_ADMIN'),(1,'ROLE_USER'),(3,'ROLE_USER'),(4,'ROLE_USER');
+/*!40000 ALTER TABLE `jhi_user_authority` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movie`
+--
+
+DROP TABLE IF EXISTS `movie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `movie` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `interest` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movie`
+--
+
+LOCK TABLES `movie` WRITE;
+/*!40000 ALTER TABLE `movie` DISABLE KEYS */;
+/*!40000 ALTER TABLE `movie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movie_wishlist`
+--
+
+DROP TABLE IF EXISTS `movie_wishlist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `movie_wishlist` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movie_wishlist`
+--
+
+LOCK TABLES `movie_wishlist` WRITE;
+/*!40000 ALTER TABLE `movie_wishlist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `movie_wishlist` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movie_wishlist_movie`
+--
+
+DROP TABLE IF EXISTS `movie_wishlist_movie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `movie_wishlist_movie` (
+  `movies_id` bigint(20) NOT NULL,
+  `movie_wishlists_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`movie_wishlists_id`,`movies_id`),
+  KEY `fk_movie_wishlist_movie_movies_id` (`movies_id`),
+  CONSTRAINT `fk_movie_wishlist_movie_movie_wishlists_id` FOREIGN KEY (`movie_wishlists_id`) REFERENCES `movie_wishlist` (`id`),
+  CONSTRAINT `fk_movie_wishlist_movie_movies_id` FOREIGN KEY (`movies_id`) REFERENCES `movie` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movie_wishlist_movie`
+--
+
+LOCK TABLES `movie_wishlist_movie` WRITE;
+/*!40000 ALTER TABLE `movie_wishlist_movie` DISABLE KEYS */;
+/*!40000 ALTER TABLE `movie_wishlist_movie` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-13 14:07:42
