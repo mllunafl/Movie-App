37b9133482d51baf652e5982b9f5648ee5247ca6
Maira <mllunaflores@gmail.com>
2017-02-22 14:23:45 -0600

    change login

--- target/generated-sources/annotations/com/example/service/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/example/service/mapper/UserMapperImpl.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Component;
 
     value = "org.mapstruct.ap.MappingProcessor",
 
-    date = "2017-02-22T12:55:20-0600",
+    date = "2017-02-22T13:24:22-0600",
 
     comments = "version: 1.1.0.Final, compiler: javac, environment: Java 1.8.0_112 (Oracle Corporation)"
 
