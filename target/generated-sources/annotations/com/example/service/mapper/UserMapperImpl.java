6f35513401c85725dfb8322aed1add45c2cbf316
Maira <mllunaflores@gmail.com>
2017-02-23 15:05:59 -0600

    fix typo in movie controller

--- target/generated-sources/annotations/com/example/service/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/example/service/mapper/UserMapperImpl.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Component;
 
     value = "org.mapstruct.ap.MappingProcessor",
 
-    date = "2017-02-23T14:54:34-0600",
+    date = "2017-02-23T14:56:33-0600",
 
     comments = "version: 1.1.0.Final, compiler: javac, environment: Java 1.8.0_112 (Oracle Corporation)"
 
